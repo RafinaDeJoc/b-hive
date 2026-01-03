064dbb31ed2b8c07470fe190fc558094f2eeedc3
rafina-de-joc <artufix@gmail.com>
2026-01-03 21:19:44 +0100

    personController added

--- b-hive-backend/src/main/java/org/example/model/Person.java
+++ b-hive-backend/src/main/java/org/example/model/Person.java
@@ -1,4 +1,69 @@
 package org.example.model;
 
+
 public class Person {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String passwordSalt;
+    private String phone;
+    private Boolean active;
+
+    public Person(String firstName, String lastName, String email, String phone, Boolean active) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.active = active;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 }
